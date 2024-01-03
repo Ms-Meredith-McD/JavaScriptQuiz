5fe4dca0ca3bbe5eda3a1e018ebc23255f7272bf
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-01-02 21:09:58 -0600

    added starting page and start button functionality, fixed console error created by last question being built, commented/framed out clearing viewport after last answer submitted

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,96 @@
+const header = document.querySelector('header');
+
+
+
 const viewport = document.querySelector('#viewport')
 
 let currentQuestion = 0
 
 const allQuestions = [
-{
-    q: "What is JavaScript?",
-    a: [
-    {text: "A language", correct: true },
-    {text: "An application", correct: false },
-    {text: "A font", correct: false },
-    {text: "A style", correct: false },
-    ]
-},
-{
-    q: "In what order is JavaScript executed?",
-    a: [
-    {text: "Right to left", correct: false },
-    {text: "Descending numerically", correct: false },
-    {text: "Ascending numerically", correct: false },
-    {text: "Top to bottom", correct: true },
-    ]
-},
-{
-    q: "What punctuation ends a line of JavaScript?",
-    a: [
-    {text: " : ", correct: false },
-    {text: " ; ", correct: true },
-    {text: " ! ", correct: false },
-    {text: " , ", correct: false },
-    ]
-},
-{
-    q: "When was JavaScript invented?",
-    a: [
-    {text: "1987", correct: false },
-    {text: "1995", correct: true },
-    {text: "1999", correct: false },
-    {text: "2002", correct: false },
-    ]
-},
-{
-    q: "What is an array?",
-    a: [
-    {text: "A list of data", correct: false },
-    {text: "A list of variables", correct: false },
-    {text: "A way to store multiple values with a single variable", correct: true },
-    {text: "A cool fish", correct: false },
-    ]
-}]
+    {
+        q: "What is JavaScript?",
+        a: [
+            { text: "A language", correct: true },
+            { text: "An application", correct: false },
+            { text: "A font", correct: false },
+            { text: "A style", correct: false },
+        ]
+    },
+    {
+        q: "In what order is JavaScript executed?",
+        a: [
+            { text: "Right to left", correct: false },
+            { text: "Descending numerically", correct: false },
+            { text: "Ascending numerically", correct: false },
+            { text: "Top to bottom", correct: true },
+        ]
+    },
+    {
+        q: "What punctuation ends a line of JavaScript?",
+        a: [
+            { text: " : ", correct: false },
+            { text: " ; ", correct: true },
+            { text: " ! ", correct: false },
+            { text: " , ", correct: false },
+        ]
+    },
+    {
+        q: "When was JavaScript invented?",
+        a: [
+            { text: "1987", correct: false },
+            { text: "1995", correct: true },
+            { text: "1999", correct: false },
+            { text: "2002", correct: false },
+        ]
+    },
+    {
+        q: "What is an array?",
+        a: [
+            { text: "A list of data", correct: false },
+            { text: "A list of variables", correct: false },
+            { text: "A way to store multiple values with a single variable", correct: true },
+            { text: "A cool fish", correct: false },
+        ]
+    }]
 console.log(allQuestions)
 console.log(allQuestions[3])
 
-function handleButtonClick(event){
-    console.log(event.target.getAttribute("data-correct"))
-    currentQuestion++
-    buildNextQuestion();
+function startButtonClick(event) {
+    const startText = document.createElement("p");
+    startText.textContent = 'Try to answer the following JavaScript related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!'
+    const startButton = document.createElement("button")
+    header.appendChild(startText);
+    startButton.textContent = 'Start'
+    header.appendChild(startButton);
+    startButton.addEventListener('click', function(){
+        buildNextQuestion()
+        header.innerHTML = ''
+    });      
 }
 
-function buildNextQuestion(){
+function handleButtonClick(event) {
+    console.log(event.target.getAttribute("data-correct"))
+    if (currentQuestion < allQuestions.length) {
+        // clear viewport
+        buildNextQuestion()
+        
+    
+}
+    else {console.log ("Game Over!")}
+    // clear viewport - create element and append an element, not use Game Over!
+};
+
+function buildNextQuestion() {
     const currentQuestionObj = allQuestions[currentQuestion]
     const questionText = currentQuestionObj.q
+    currentQuestion++
+    
 
     const pTag = document.createElement("p");
     pTag.textContent = questionText
     viewport.appendChild(pTag)
 
-    const buttons = currentQuestionObj.a.map( function(obj){
+    const buttons = currentQuestionObj.a.map(function (obj) {
         const btn = document.createElement("button");
         btn.textContent = obj.text
         btn.setAttribute("data-correct", obj.correct)
@@ -73,5 +98,4 @@ function buildNextQuestion(){
         viewport.appendChild(btn)
     })
 }
-
-buildNextQuestion();
+startButtonClick();
